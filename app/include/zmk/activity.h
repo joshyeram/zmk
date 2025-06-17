a80a16de4a8b13b69821efd55816d7942c47487a
Joshua Chung <42791673+joshyeram@users.noreply.github.com>
2025-06-17 01:59:07 -0400

    Update activity.h

--- app/include/zmk/activity.h
+++ app/include/zmk/activity.h
@@ -6,6 +6,6 @@
 
 #pragma once
 
-enum zmk_activity_state { ZMK_ACTIVITY_ACTIVE, ZMK_ACTIVITY_IDLE, ZMK_ACTIVITY_SLEEP };
+enum zmk_activity_state { ZMK_ACTIVITY_ACTIVE, ZMK_ACTIVITY_IDLE, ZMK_ACTIVITY_SLEEP, ZMK_ACTIVITY_PAST_IDLE};
 
 enum zmk_activity_state zmk_activity_get_state(void);
